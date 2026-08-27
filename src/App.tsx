8a27d9f505774baa972a960101b3d319e9499cba
jaque <jaquelinevieira1010@outlook.com>
2026-08-27 12:53:46 -0300

    chore: update app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from 'react-router';
 import { ToastProvider } from './contexts/ToastContext';
-import { SignupProvider } from './contexts/SignupContext';
 import AppRoutes from './routes/AppRoutes';
 
 export default function App() {
   return (
     <BrowserRouter>
       <ToastProvider>
-        <SignupProvider>
-          <AppRoutes />
-        </SignupProvider>
+        <AppRoutes />
       </ToastProvider>
     </BrowserRouter>
   );
